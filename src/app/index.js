280857d6325514934ee806d5d98e3b2665e530da
Eduardo López <spoke.asafmexico@hotmail.com>
2023-08-09 22:57:29 -0600

    Cambio de idioma en API

--- src/app/index.js
+++ src/app/index.js
@@ -8,7 +8,7 @@ class Weather {
     
 
     async getWeather() {
-        const URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apikey}&units=metric`;
+        const URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apikey}&units=metric&lang=es`;
         const response = await fetch(URI);
         const data = await response.json();
         return data;
